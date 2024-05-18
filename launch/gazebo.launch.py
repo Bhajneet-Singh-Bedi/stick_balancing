5ca1682a122e8ab7f481d256f711d3c9ddd344c6
Bhajneet Singh Bedi <bhajneetbedi2002@gmail.com>
2024-05-18 21:22:00 +0530

    added stick.xacro

--- launch/gazebo.launch.py
+++ launch/gazebo.launch.py
@@ -1,69 +1,44 @@
-from launch_ros.actions import Node
-from launch_ros.substitutions import FindPackageShare
-from launch import LaunchDescription
-from launch.conditions import IfCondition, UnlessCondition
-from launch.actions import IncludeLaunchDescription ,DeclareLaunchArgument
-from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import PathJoinSubstitution,LaunchConfiguration, PythonExpression
 import os
 import xacro
-from ament_index_python.packages import get_package_share_directory,get_package_prefix
-
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.substitutions import LaunchConfiguration
+from launch.actions import DeclareLaunchArgument
+from ament_index_python.packages import get_package_share_directory
+from launch.actions import ExecuteProcess
 
 def generate_launch_description():
-    share_dir = get_package_share_directory('stick_balancing')
 
-    xacro_file = os.path.join(share_dir, 'urdf','stick.xacro')
-    assert os.path.exists(xacro_file), "The stick.xacro file does not exists in "+str(xacro_file)
+  # Replace with the actual name of your robot description package (assuming it's within the workspace)
+  robot_description_package = 'stick_balancing'
 
-    robot_description_config = xacro.process_file(xacro_file)
-    robot_urdf = robot_description_config.toxml()
-    print(robot_urdf)
-    world_file_name = 'gazebo.world'
-    world_path = os.path.join(share_dir, 'worlds','gazebo.world'), ''
-    
-    install_dir = get_package_prefix('stick_balancing')
+  # Path to the URDF file within the package
+  xacro_file = os.path.join(get_package_share_directory('stick_balancing'), 'urdf', 'stick.xacro')  
+  assert os.path.exists(xacro_file), "The builder_bot.xacro doesnt exist in "+str(xacro_file) 
+#   robot_description_file = robot_description_package + '/urdf/box.xacro'
   
-    world = LaunchConfiguration('world')
-    
+  # Launch arguments for Gazebo
+  use_sim_time = LaunchConfiguration('use_sim_time', default='true')
+  gui = LaunchConfiguration('gui', default='true')  # Optional (headless or GUI)
 
- 
-    declare_world_cmd = DeclareLaunchArgument(
-        name='world',
-        default_value=world_path,
-        description='Full path to the world model file to load')
- 
+  gazebo_world_path = os.path.join(get_package_share_directory('stick_balancing'), 'worlds', 'empty_world.world')
 
 
+  return LaunchDescription([
+      DeclareLaunchArgument('use_sim_time', default_value='true', description='Use simulation (Gazebo) clock if true'),
+      DeclareLaunchArgument('gui', default_value='true', description='Start Gazebo in GUI mode'),
 
-    gazebo_server = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource([
-            PathJoinSubstitution([
-                FindPackageShare('gazebo_ros'),
-                'launch',
-                'gzserver.launch.py'
-            ])
-        ]),
-        launch_arguments={
-            'pause': 'false',
-             world: 'world'
-        }.items()
-    )
-
-    gazebo_client = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource([
-            PathJoinSubstitution([
-                FindPackageShare('gazebo_ros'),
-                'launch',
-                'gzclient.launch.py'
-            ])
-        ])
-    )
-
-
-
-    return LaunchDescription([
-        declare_world_cmd, 
-        gazebo_server,
-        gazebo_client,
-    ])
+      # Launch Gazebo with the empty world
+      ExecuteProcess(
+        cmd=['gazebo', '--verbose', gazebo_world_path],
+        output='screen'
+      ),
+      # Spawn the box model using robot_state_publisher
+      Node(
+          package='robot_state_publisher',
+          executable='robot_state_publisher',
+          name='robot_state_publisher',
+          output='screen',
+          parameters=[{'robot_description': xacro.process_file(xacro_file).toxml()}]
+      )
+  ])
